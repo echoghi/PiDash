1a0f01c75270ed0f8f5f4fe73b428b5eed35c9e3
Emile Choghi <emchoghi@gmail.com>
2016-04-13 00:08:07 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,7 @@
 (function() {
   angular.module('dashboard', [])
 
-  .controller("weather", ['$scope', '$http', function($scope, $http) {
-    $scope.weather,
-      $scope.time,
-      $scope.temp,
-      $scope.min,
-      $scope.max,
-      $scope.min2,
-      $scope.max2,
-      $scope.min3,
-      $scope.max3,
-      $scope.min4,
-      $scope.max4,
-      $scope.min5,
-      $scope.max5,
-      $scope.min6,
-      $scope.max6;
+  .controller("weather", ['$scope', '$http', '$interval', function($scope, $http, $interval) {
 
     // Get the Day
     var days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
@@ -30,69 +15,87 @@
     $scope.forecastDay3 = days[n + 3];
     $scope.forecastDay4 = days[n + 4];
     $scope.forecastDay5 = days[n + 5];
-    $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=paloalto,us&APPID=1c3673cc09eb008cb08f2075c97393ae&cnt=6").success(function(data) {
-      $scope.weather = data;
-      $scope.temp = Math.floor(data.list[0].temp.day * 9 / 5 - 459.67) + "°F";
-      $scope.min = Math.floor(data.list[0].temp.min * 9 / 5 - 459.67);
-      $scope.max = Math.floor(data.list[0].temp.max * 9 / 5 - 459.67);
-      $scope.min2 = Math.floor(data.list[1].temp.min * 9 / 5 - 459.67);
-      $scope.max2 = Math.floor(data.list[1].temp.max * 9 / 5 - 459.67);
-      $scope.min3 = Math.floor(data.list[2].temp.min * 9 / 5 - 459.67);
-      $scope.max3 = Math.floor(data.list[2].temp.max * 9 / 5 - 459.67);
-      $scope.min4 = Math.floor(data.list[3].temp.min * 9 / 5 - 459.67);
-      $scope.max4 = Math.floor(data.list[3].temp.max * 9 / 5 - 459.67);
-      $scope.min5 = Math.floor(data.list[4].temp.min * 9 / 5 - 459.67);
-      $scope.max5 = Math.floor(data.list[4].temp.max * 9 / 5 - 459.67);
-      $scope.min6 = Math.floor(data.list[5].temp.min * 9 / 5 - 459.67);
-      $scope.max6 = Math.floor(data.list[5].temp.max * 9 / 5 - 459.67);
-    })
+
+    function weatherUpdate() {
+      $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=paloalto,us&APPID=1c3673cc09eb008cb08f2075c97393ae&cnt=6").success(function(data) {
+        $scope.weather = data;
+        $scope.temp = Math.floor(data.list[0].temp.day * 9 / 5 - 459.67) + "°F";
+        $scope.min = Math.floor(data.list[0].temp.min * 9 / 5 - 459.67);
+        $scope.max = Math.floor(data.list[0].temp.max * 9 / 5 - 459.67);
+        $scope.min2 = Math.floor(data.list[1].temp.min * 9 / 5 - 459.67);
+        $scope.max2 = Math.floor(data.list[1].temp.max * 9 / 5 - 459.67);
+        $scope.min3 = Math.floor(data.list[2].temp.min * 9 / 5 - 459.67);
+        $scope.max3 = Math.floor(data.list[2].temp.max * 9 / 5 - 459.67);
+        $scope.min4 = Math.floor(data.list[3].temp.min * 9 / 5 - 459.67);
+        $scope.max4 = Math.floor(data.list[3].temp.max * 9 / 5 - 459.67);
+        $scope.min5 = Math.floor(data.list[4].temp.min * 9 / 5 - 459.67);
+        $scope.max5 = Math.floor(data.list[4].temp.max * 9 / 5 - 459.67);
+        $scope.min6 = Math.floor(data.list[5].temp.min * 9 / 5 - 459.67);
+        $scope.max6 = Math.floor(data.list[5].temp.max * 9 / 5 - 459.67);
+      })
+    }
+    weatherUpdate();
+    $interval(function() {
+      weatherUpdate();
+    }, 120000);
 
   }])
 
-  .controller("reddit", ['$scope', '$http', function($scope, $http) {
-    $scope.photos;
-    $scope.reddit;
-    $scope.title;
-    $http.get("https://www.reddit.com/r/earthporn/.json").success(function(data) {
-      $scope.reddit = data;
-      $scope.photos = data.data.children[0].data.preview.images[0].source.url;
-      $scope.title = data.data.children[0].data.title;
-    });
+  .controller("reddit", ['$scope', '$http', '$interval', function($scope, $http, $interval) {
 
+    function redditUpdate() {
+      $http.get("https://www.reddit.com/r/earthporn/.json").success(function(data) {
+        $scope.reddit = data;
+        $scope.photos = data.data.children[0].data.preview.images[0].source.url;
+        $scope.title = data.data.children[0].data.title;
+      });
+    }
+    redditUpdate();
+    $interval(function() {
+      redditUpdate();
+    }, 120000);
   }])
 
-  .controller("news", ['$scope', '$http', function($scope, $http) {
-    $scope.news;
-    $http.get("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1/.json?api-key=da100d8e23d3f94fc1ff6deefe9742ea:2:70699191").success(function(data) {
-      $scope.news = data;
-    });
+  .controller("news", ['$scope', '$http', '$interval', function($scope, $http, $interval) {
+
+    function newsUpdate() {
+      $http.get("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1/.json?api-key=da100d8e23d3f94fc1ff6deefe9742ea:2:70699191").success(function(data) {
+        $scope.news = data;
+      });
+    }
+    newsUpdate();
+    $interval(function() {
+      newsUpdate();
+    }, 120000);
+
   }])
 
   .controller("ethereum", ['$scope', '$http', '$interval', function($scope, $http, $interval) {
-    $scope.ether;
-    
-function etherUpdate(){
- $http.get("https://coinmarketcap-nexuist.rhcloud.com/api/eth").success(function(data) {
-      $scope.ether = data;
-   $scope.supply = (data.supply).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      $scope.market_cap = (data.market_cap.usd).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-   $scope.price = (data.price.usd).toFixed(2);
-   $scope.btc = (data.price.btc);
-   if(+data.change < 0){
-     $('#change').css('color', 'red');
-   }
-   if(+data.change > 0){
-      $('#change').css('color', 'green');
-   }
-    }); }
-    
+
+    function etherUpdate() {
+      $http.get("https://coinmarketcap-nexuist.rhcloud.com/api/eth").success(function(data) {
+        $scope.ether = data;
+        $scope.supply = (data.supply).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        $scope.market_cap = (data.market_cap.usd).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        $scope.price = (data.price.usd).toFixed(2);
+        $scope.btc = (data.price.btc);
+        if (+data.change < 0) {
+          $('#change').css('color', 'red');
+        }
+        if (+data.change > 0) {
+          $('#change').css('color', 'green');
+        }
+      });
+    }
+
     etherUpdate();
-    $interval(function() { etherUpdate();}, 120000);
+    $interval(function() {
+      etherUpdate();
+    }, 120000);
 
   }])
 
   .controller("clock", ['$scope', '$interval', function($scope, $interval) {
-    $scope.time;
 
     function updateTime() {
       var currentDate = new Date(),
@@ -127,8 +130,8 @@ function etherUpdate(){
       }
       return realTime;
     }
-   $interval(function() {
-        $scope.time = updateTime();
+    $interval(function() {
+      $scope.time = updateTime();
     }, 1000);
   }])
 
