cbb79bfaa3a25568943f47caf565571b45dfba9b
Emile Choghi <emchoghi@gmail.com>
2016-04-12 18:21:57 -0700

    Update and rename js/app.js to app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var app = require('express')(),
+var express = require('express'),
+app = express(),
 server = require('http').Server(app);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(__dirname));
 
 server.listen(3000, function(){
   console.log('listening on port 3000');
